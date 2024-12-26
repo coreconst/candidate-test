05ad746ab7454c3e04902bc6e61e6ebca2918825
constantine <parfilokostya0@gmail.com>
2024-12-26 21:33:04 +0200

    Add label method to TestStatus enum

--- app/Enums/TestStatus.php
+++ app/Enums/TestStatus.php
@@ -2,8 +2,15 @@
 
 namespace App\Enums;
 
+use Illuminate\Support\Str;
+
 enum TestStatus: string
 {
     case InProgress = 'in_progress';
     case Completed = 'completed';
+
+    public function label(): string
+    {
+        return Str::of($this->value)->replace('_', ' ')->title();
+    }
 }
