55119ad1c1a8ccbb1f6c2e10126fc86f2a7b4013
constantine <parfilokostya0@gmail.com>
2024-12-26 11:51:44 +0200

    Add table and textarea components

---
+++ resources/js/Components/Table.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps(['columns'])
+</script>
+
+<template>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+            <th v-for="column in columns" class="px-6 py-3">
+                {{ column }}
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+            <slot />
+        </tbody>
+    </table>
+</template>
